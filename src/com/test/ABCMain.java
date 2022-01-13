e5f28c67b95f74116bd1555b512c2eaba40aec88
Siva Kamesh <sivakamesh1@gmail.com>
2022-01-14 06:12:32 +0800

    This is for poll SCM test..

--- src/com/test/ABCMain.java
+++ src/com/test/ABCMain.java
@@ -3,6 +3,6 @@ package com.test;
 public class ABCMain {
 
 	public static void main(String[] args) {
-		System.out.println(".............Hell This is Fist Project ABC...............");
+		System.out.println(".............Hell This is Fist Project ABC.................");
 	}
 }
